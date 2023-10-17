f7dda0c67345bafbd54281975d925ff32c1f20a6
noahbetten <duc@bluespacecreative.com>
2023-10-17 17:24:46 -0500

    creating the navigation bar

--- components/GlobalConfig.js
+++ components/GlobalConfig.js
@@ -1,20 +1,48 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react"
 import Link from "next/link"
+import { useState } from "react"
 const GlobalConfig = ({ blok }) => {
-  return (
-    <div className="relative bg-white border-b-2 border-gray-100" {...storyblokEditable(blok)}>
-      <div className="px-4 mx-auto max-w-7xl sm:px-6">
-        <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
-          <div className="flex justify-start lg:w-0 lg:flex-1">
-            <Link href="/">My Logo</Link>
-          </div>
+  const [isExpanded, setIsExpanded] = useState(false)
 
-          {blok.headerMenu.map((nestedBlok) => (
-            <StoryblokComponent className="" blok={nestedBlok} key={nestedBlok._uid} />
-          ))}
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded)
+  }
+
+  return (
+    <>
+      <nav className="p-4 bg-blue-500" {...storyblokEditable(blok)}>
+        <div className="container mx-auto">
+          <div className="flex items-center justify-between">
+            <Link href="/" className="text-2xl font-bold text-white">
+              My Website
+            </Link>
+            {/* menu */}
+            <div className="hidden space-x-4 md:flex">
+              {blok.headerMenu.map((nestedBlok) => (
+                <StoryblokComponent className="" blok={nestedBlok} key={nestedBlok._uid} />
+              ))}
+            </div>
+            {/* button */}
+            <button className="text-white md:hidden" onClick={toggleExpansion}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </nav>
+
+      {/* mobile menu */}
+      <div className="bg-blue-500 md:hidden" style={{ display: !isExpanded && "none" }}>
+        <div className="container mx-auto">
+          <div className="flex flex-col p-4 space-y-4">
+            {blok.headerMenu.map((nestedBlok) => (
+              <StoryblokComponent className="" blok={nestedBlok} key={nestedBlok._uid} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 export default GlobalConfig
--- components/MenuLink.js
+++ components/MenuLink.js
@@ -1,7 +1,7 @@
 import { storyblokEditable } from "@storyblok/react"
 import Link from "next/link"
 const MenuLink = ({ blok }) => (
-  <Link className="text-base font-medium text-gray-500 hover:text-gray-900" href={`/${blok.link.cached_url}`} {...storyblokEditable(blok)}>
+  <Link class=" text-white hover:text-gray-300" href={`/${blok.link.cached_url}`} {...storyblokEditable(blok)}>
     {blok.name}
   </Link>
 )
